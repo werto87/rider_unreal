c4cb3f104947656e673a70e2dc2945bce05a0c19
werto87 <huhulasmichinruhe@web.de>
2022-09-23 10:23:00 +0900

    Initial commit

---
+++ Source/MyProject6/MyProject6GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProject6GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT6_API AMyProject6GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
